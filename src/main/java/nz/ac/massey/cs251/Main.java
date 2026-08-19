80cbd50c68369dea67709a18d5ae8b34c71caa62
tri-iisha <trishachand10@gmail.com>
2026-08-19 20:49:23 +1200

    Added three menu buttons with sub menusu

--- src/main/java/nz/ac/massey/cs251/Main.java
+++ src/main/java/nz/ac/massey/cs251/Main.java
@@ -1,17 +1,69 @@
 package nz.ac.massey.cs251;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.*;
+import java.awt.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main extends JFrame{
+    private static final int Frame_WIDTH = 800;
+    private static final int Frame_HEIGHT = 600;
+
+    private JTextArea textArea;
+    private JMenuItem selectText, copy, paste, cut;
+    private JLabel infoLabel;
+
+    public Main(){
+        super("Text Editor");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(Frame_WIDTH, Frame_HEIGHT);
+        this.setResizable(true);
+
+        JMenuBar MenuBar = new JMenuBar();
+        this.setJMenuBar(MenuBar);
+
+        JMenu fileMenu = new JMenu("File");
+        JMenu viewMenu = new JMenu("View");
+        JMenu helpMenu = new JMenu("Help");
+
+        JMenuItem newItem = new JMenuItem("New");
+        JMenuItem openItem = new JMenuItem("Open");
+        JMenuItem saveItem = new JMenuItem("Save");
+        JMenuItem printItem = new JMenuItem("Print");
+        JMenuItem exportItem = new JMenuItem("Export as PDF");
+        JMenuItem exitItem = new JMenuItem("Exit");
+
+        JMenuItem dateTimeItem = new JMenuItem("Date & Time");
+
+        JMenuItem aboutItem = new JMenuItem("About");
+
+        fileMenu.add(newItem);
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(printItem);
+        fileMenu.add(exportItem);
+        fileMenu.addSeparator();
+        fileMenu.add(exitItem);
+
+        viewMenu.add(dateTimeItem);
+
+        helpMenu.add(aboutItem);
+
+        MenuBar.add(fileMenu);
+        MenuBar.add(viewMenu);
+        MenuBar.add(helpMenu);
+
+
+
+
+        LocalDateTime time = LocalDateTime.now();
+
+        infoLabel= new JLabel("Time & Date");
+        MenuBar.add(infoLabel);
+        infoLabel.setText("Time" + time);
+
+        this.setVisible(true);
     }
+
+    public static void main(String[] args){ new Main(); }
 }
