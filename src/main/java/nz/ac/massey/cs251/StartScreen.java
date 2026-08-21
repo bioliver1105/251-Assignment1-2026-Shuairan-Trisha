a868436413adef919a114bf26cc68fae6983c0e4
tri-iisha <trishachand10@gmail.com>
2026-08-22 01:17:53 +1200

    Changed background colors and added start screen with functional start button

---
+++ src/main/java/nz/ac/massey/cs251/StartScreen.java
@@ -0,0 +1,32 @@
+package nz.ac.massey.cs251;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StartScreen extends JFrame {
+
+    public StartScreen() {
+        super("Text Editor");
+        setSize(800,500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        JLabel background = new JLabel(new ImageIcon("start4.png"));
+        background.setLayout(null);
+
+        JButton startButton = new JButton("Start");
+        startButton.setBounds(350,340,100,40);
+        startButton.setBackground(new Color(70, 217, 192));
+        startButton.setForeground(new Color(20,30,50));
+        startButton.setFocusPainted(false);
+        startButton.addActionListener(e -> {
+            this.dispose();
+            SwingUtilities.invokeLater(Main::new);
+        });
+
+        background.add(startButton);
+        this.add(background);
+        this.setVisible(true);
+    }
+}
